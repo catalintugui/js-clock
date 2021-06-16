6a7960ef5e70723fd304fa574a13e16c2eb9e1b7
Catalin Tugui <katamaxx23@gmail.com>
2021-06-16 18:26:05 +0300

    transition fix

--- script.js
+++ script.js
@@ -5,14 +5,27 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setDate(){
     const now = new Date();
-
+    
     const seconds = now.getSeconds();
     const min = now.getMinutes();
     const hour = now.getHours();
 
+    if(seconds==0){
+        secondHand.style.transitionDuration = '0s';
+        minHand.style.transitionDuration = '0s';
+        hourHand.style.transitionDuration = '0s';
+    } else {
+        secondHand.style.transitionDuration = '0.05s';
+        minHand.style.transitionDuration = '0.05s';
+        hourHand.style.transitionDuration = '0.05s';
+    }
+    
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     const minDegrees = ((min / 60) * 360) + 90;
     const hourDegrees = ((hour / 12) * 360) + 90;
+
+    console.log(secondsDegrees);
+    console.log(minDegrees);
     
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     minHand.style.transform = `rotate(${minDegrees}deg)`;
